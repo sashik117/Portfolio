9ab5578b1871875d31d5d1cc1f22ea4e9329802d
sashik117 <sanyoklolik@gmail.com>
2026-04-09 00:36:35 +0300

    update with admin panel

--- src/components/admin/AdminExperience.jsx
+++ src/components/admin/AdminExperience.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { supabase } from "@/lib/supabase";
+import { supabase } from "@/api/SupabaseClient";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
